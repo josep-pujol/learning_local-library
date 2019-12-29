9c89c91d44e0845ccb23ef657f6ba104919a8063
josep-pujol <pujol.rabat@gmail.com>
2019-12-29 01:15:37 +0000

    added list filters for a few Admin Classes. Added inline instances for BookAdmin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,54 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance
 
-admin.site.register(Book)
-admin.site.register(Author)
+# originally models were only registered. Commented out when added admin classes
+# admin.site.register(Book) 
+# admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+# admin.site.register(BookInstance)
+
+# Basic way to registar an admin class. Below you see with decorators
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+
+# To add inline info to Book Django admin page
+# Adds all instances of a specific Book (BookInstance) in the
+# BooksAdmin page, right below the book info
+class BooksInstanceInline(admin.TabularInline):
+    """Defines format of inline book instance insertion (used in BookAdmin)"""
+    # Notice this only add all BookInstances in a specific BookAdmin
+    # Ideally it should only display the instances of that particular book
+    model = BookInstance
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    """Administration object for Book models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - adds inline addition of book instances in book view (inlines)
+    """
+    list_display = ('title', 'author', 'display_genre')
+    # To add inlines in Book page of Django admin
+    inlines = [BooksInstanceInline]
+
+
+@admin.register(BookInstance) 
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
--- catalog/models.py
+++ catalog/models.py
@@ -29,9 +29,12 @@ class Book(models.Model):
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
 
-    def __str__(self):
-        """String for representing the Model object."""
-        return self.title
+    def display_genre(self):
+        """Because one book can have multiple genres, creates a string with the first 3 Genres.
+         This is required to display genre in Admin."""
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+
+    display_genre.short_description = 'Genre'
 
     # get_absolute_url() returns a URL that can be used to access a detail record for this model
     # for this to work we will have to define a URL mapping that has the name book-detail,
@@ -39,6 +42,10 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
 
 
 class BookInstance(models.Model):
@@ -49,7 +56,7 @@ class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True) 
-    imprint = models.CharField(max_length=200, help_text='version details of the book')
+    imprint = models.CharField(max_length=200, help_text='version details of a book')
     due_back = models.DateField(null=True, blank=True)
 
     LOAN_STATUS = (
