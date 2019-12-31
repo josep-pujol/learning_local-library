3898255328d6333e716843d88c5cab08ba1f2576
josep-pujol <pujol.rabat@gmail.com>
2019-12-31 00:34:34 +0000

    created home/index page

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     # Add URL maps to redirect the base URL to our application
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
 
-    # Use static() to add url mapping to serve static files during development (only)
+    # Use static() to add url mapping to serve static files during
+    # development (only)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
