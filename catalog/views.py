5da6baf318ceecf3e65634cb69860d5e4c6930d7
josep-pujol <pujol.rabat@gmail.com>
2020-01-05 21:46:02 +0000

    added a counter of the times browser visited a page using sessions

--- catalog/views.py
+++ catalog/views.py
@@ -20,6 +20,11 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    # Number of visits to this view, as counted in the session variable.
+    print('\nbefore', request.session.keys())
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    print('after:', request.session.keys())
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
@@ -27,6 +32,7 @@ def index(request):
         'num_authors': num_authors,
         'num_genres': num_genres,
         'num_books_w_z': num_books_w_z,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
