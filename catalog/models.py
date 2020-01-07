9e1679c11d3526c2945e711d8b039ff41d775485
josep-pujol <pujol.rabat@gmail.com>
2020-01-07 01:06:00 +0000

    linked User with BookInstance models for the borrow books functionality

--- catalog/models.py
+++ catalog/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from django.urls import reverse
 # Required for unique book instances
 import uuid
+from django.contrib.auth.models import User
+from datetime import date
 
 
 class Genre(models.Model):
@@ -57,9 +59,16 @@ class BookInstance(models.Model):
 
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
-    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True) 
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200, help_text='version details of a book')
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
