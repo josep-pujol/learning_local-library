471b9f2650e77bcfc98ef98b8f831ddfe3e5fbc9
josep-pujol <pujol.rabat@gmail.com>
2020-01-11 21:48:03 +0000

    added create, edit, delete operations for authors

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,14 +2,25 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Function view
+    # Exmaple of a function view for the index page
     path('', views.index, name='index'),
-    # Class view
+
+    # Example of class views for listing books
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+
+    # Listing Authors
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+
+    # Listing borrowed books
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
     path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+
+    # CRUD operations with Author
+    path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
+
 ]
